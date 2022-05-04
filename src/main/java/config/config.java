dba7fdfe2607c095dd5ef2290dce48de030f060d
AXEwiges <2495477644@qq.com>
2022-05-04 19:18:41 +0800

    load YAML config

---
+++ src/main/java/config/config.java
@@ -0,0 +1,60 @@
+package config;
+
+import lombok.AllArgsConstructor;
+import org.yaml.snakeyaml.Yaml;
+import java.io.InputStream;
+import java.util.Map;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+class metadata{
+    public boolean isMaster;
+    public int uid;
+}
+
+@Data
+@AllArgsConstructor
+class cluster{
+    public String mainTable;
+    public int maxTables;
+    public int neighbor;
+}
+
+@Data
+@AllArgsConstructor
+class network{
+    public int timeOut;
+    public String ip;
+    public int port;
+}
+
+@Data
+public class config {
+    public String appVersion;
+    public metadata metadata;
+    public cluster cluster;
+    public network network;
+
+    public void loadYaml(){
+        Yaml yaml = new Yaml();
+        InputStream in = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("config/server.yaml");
+        Map<String, Object> obj = yaml.load(in);
+        Map<String, Object> metadata = (Map<String, Object>) obj.get("metadata");
+        Map<String, Object> cluster = (Map<String, Object>) obj.get("cluster");
+        Map<String, Object> network = (Map<String, Object>) obj.get("network");
+
+        this.appVersion = (String) obj.get("appVersion");
+        this.metadata = new metadata((boolean)metadata.get("isMaster"), (int)metadata.get("uid"));
+        this.cluster = new cluster((String)cluster.get("mainTable"), (int)cluster.get("maxTables"), (int)cluster.get("neighbor"));
+        this.network = new network((int)network.get("timeOut"), (String)network.get("ip"), (int)network.get("port"));
+    }
+
+    public static void main(String[] args) {
+        config a = new config();
+        a.loadYaml();
+        System.out.println(a);
+    }
+}
