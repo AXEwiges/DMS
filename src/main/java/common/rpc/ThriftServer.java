8509a0d2594b8d6f901b7cb34066d078efc54fb5
AXEwiges <2495477644@qq.com>
2022-05-06 21:36:20 +0800

    timed upload

--- src/main/java/common/rpc/ThriftServer.java
+++ src/main/java/common/rpc/ThriftServer.java
@@ -1,10 +1,26 @@
 package common.rpc;
 
 import org.apache.thrift.TProcessor;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TTransportException;
 
 public class ThriftServer {
 
-    public ThriftServer()
+    private TServer server;
+    public int port;
+    private TProcessor processor;
+
+    public ThriftServer(TProcessor processor, int port) throws TTransportException {
+        this.port = port;
+        TServerTransport serverTransport = new TServerSocket(9090);
+        TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+        server.serve();
+
+    }
+
     public void startServer() {
 
     }
