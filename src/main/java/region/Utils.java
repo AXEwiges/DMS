5ce8e462bf50dad74100fce3c0981da236a07eac
AXEwiges <2495477644@qq.com>
2022-05-16 20:58:37 +0800

    Update Utils.java

--- src/main/java/region/Utils.java
+++ src/main/java/region/Utils.java
@@ -1,5 +1,5 @@
 package region;
 
 public class Utils {
-    public static String DBFiles = "E:\\SQL\\DMS\\src\\main\\java\\region\\db\\DBFiles\\";
+    public static String DBFiles = "D:\\RDBMS\\DMS\\src\\main\\java\\region\\db\\DBFiles\\";
 }
