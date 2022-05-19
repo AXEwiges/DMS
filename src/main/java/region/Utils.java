62cd12db042b56d031c1a7d208b21bf34c476c3d
Smh <76023060+Kona-Sumon@users.noreply.github.com>
2022-05-19 15:31:57 +0800

    update

--- src/main/java/region/Utils.java
+++ src/main/java/region/Utils.java
@@ -4,6 +4,6 @@ public class Utils {
     /**
      * 物理机的数据文件存储路径
      * */
-    public static String DBFiles = "E:\\SQL\\DMS\\src\\main\\java\\region\\db\\DBFiles\\";
-//    public static String DBFiles = "D:\\RDBMS\\DMS\\src\\main\\java\\region\\db\\DBFiles\\";
+//    public static String DBFiles = "E:\\SQL\\DMS\\src\\main\\java\\region\\db\\DBFiles\\";
+    public static String DBFiles = "D:\\RDBMS\\DMS\\src\\main\\java\\region\\db\\DBFiles\\";
 }
